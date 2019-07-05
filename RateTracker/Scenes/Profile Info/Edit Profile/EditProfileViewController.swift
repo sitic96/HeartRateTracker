277869a58158a623b0ca9d9c3b0aa48cf3ebb3a4
Sitora Guliamova <redastify@gmail.com>
2019-07-05 12:35:00 +0300

    edit-info: implemented ui for edit info view controller

--- RateTracker/Scenes/Profile Info/Edit Profile/EditProfileViewController.swift
+++ RateTracker/Scenes/Profile Info/Edit Profile/EditProfileViewController.swift
@@ -9,14 +9,26 @@
 import UIKit
 
 class EditProfileViewController: UIViewController {
+    @IBOutlet weak var profilePhotoImageView: UIImageView!
+    @IBOutlet weak var dateOfBirthPickerView: UIDatePicker!
+    @IBOutlet weak var secondNameTextField: UITextField!
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var emailTextField: UITextField!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        dateOfBirthPickerView.maximumDate = Date()
     }
 
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
 
         navigationController?.isNavigationBarHidden = true
+    }
+
+    @IBAction func userDidSelectUpdatePhoto(_ sender: Any) {
+    }
+
+    @IBAction func userDidFinishEditProfile(_ sender: Any) {
     }
 }
