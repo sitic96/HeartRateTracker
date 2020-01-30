1839c8bc041ca7e2eb34ebf17caea6dd32ab97c4
Sitora Guliamova <redastify@gmail.com>
2020-01-30 16:13:38 +0300

    ble-support: created main vc, setup project structure

---
+++ RateTracker/Presentation/Modules/Main/PastRecords/PastRecordsViewController.swift
@@ -0,0 +1,16 @@
+//
+//  PastRecordsViewController.swift
+//  RateTracker
+//
+//  Created by Sitora on 30/01/2020.
+//  Copyright © 2020 Sitora Guliamova. All rights reserved.
+//
+
+import UIKit
+
+class PastRecordsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
