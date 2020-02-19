537457c65a4311df2668fa56abbfd4f18899ad7a
Sitora Guliamova <redastify@gmail.com>
2020-02-19 21:07:39 +0300

    scenes/profile-info: setup ui for profile info tab

--- RateTracker/Presentation/Modules/Main/Profile/ProfileViewController.swift
+++ RateTracker/Presentation/Modules/Main/Profile/ProfileViewController.swift
@@ -9,11 +9,21 @@
 import UIKit
 
 class ProfileViewController: UIViewController {
+    @IBOutlet private weak var profileContainerView: UIView!
+    @IBOutlet private weak var profileImageView: UIImageView!
+    @IBOutlet private weak var userNameLabel: UILabel!
 
     var viewModel: ProfileViewModelProtocol!
 
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         viewModel.viewDidAppear()
+    }
+
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        profileContainerView.cornerRadius = profileContainerView.frame.height / 2
+        profileContainerView.viewBorderWidth = 3
+        userNameLabel.text = "Sitora Guliamova"
     }
 }
